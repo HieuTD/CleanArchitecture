c70c84836dccd7c0a4f583d6cdbb4396c3ff2374
HieuTD <hieutrinhduc1612@gmail.com>
2025-03-24 14:53:27 +0700

    Add Redis to project (BlogService)

--- CleanArchitecture.Infrastructure/Services/BlogService.cs
+++ CleanArchitecture.Infrastructure/Services/BlogService.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using CleanArchitecture.Application.DTOs.Blogs;
 using CleanArchitecture.Application.Interfaces.Repositories;
 using CleanArchitecture.Application.Interfaces.Services;
 using CleanArchitecture.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
 
 namespace CleanArchitecture.Infrastructure.Services
 {
     public class BlogService : IBlogService
     {
         private readonly IBlogRepository _blogRepository;
+        private readonly IDatabase _cache;
+        private readonly int _cacheExpirationMinutes;
 
-        public BlogService(IBlogRepository blogRepository) 
+        public BlogService(IBlogRepository blogRepository, IConfiguration configuration) 
         {
             _blogRepository = blogRepository;
+            var redis = ConnectionMultiplexer.Connect(configuration["Redis:ConnectionString"]);
+            _cache = redis.GetDatabase();
+            _cacheExpirationMinutes = int.Parse(configuration["Redis:CacheExpirationMinutes"]);
         }
         public async Task AddBlogAsync(BlogCreateRequest request)
         {
@@ -55,14 +63,28 @@ namespace CleanArchitecture.Infrastructure.Services
 
         public async Task<IEnumerable<BlogViewModel>> GetAllBlogsAsync()
         {
-            var blogs = await _blogRepository.GetAllBlogsAsync();
-            return blogs.Select(b => new BlogViewModel
+            string cacheKey = "blog_list";
+            string cachedData = await _cache.StringGetAsync(cacheKey);
+
+            //Kiểm tra redis có sẵn dữ liệu blog không (với key = "blog_list") nếu có thì lấy từ redis, nếu không thì lấy từ repo
+            if (!String.IsNullOrEmpty(cachedData))
+            {
+                return JsonSerializer.Deserialize<List<BlogViewModel>>(cachedData);
+            }
+            var blogsRepo = await _blogRepository.GetAllBlogsAsync();
+
+            var blogs = blogsRepo.Select(b => new BlogViewModel
             {
                 Id = b.Id,
                 Description = b.Description,
                 UserName = b.AppUser.FirstName,
                 CreatedAt = DateTime.Now
             });
+
+            //Thêm dữ liệu vào redis (với key = "blog_list") | set thời gian tự động xóa cache sau 10' (_cacheExpirationMinutes)
+            await _cache.StringSetAsync(cacheKey, JsonSerializer.Serialize(blogs), TimeSpan.FromMinutes(_cacheExpirationMinutes));
+
+            return blogs;
         }
 
         public async Task UpdateBlogAsync(int id, BlogCreateRequest request)
